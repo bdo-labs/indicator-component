5fe04d8837cb5fa55dd12e129c91dba9adf1f0b6
Péter Gombos <pego@bdo.no>
2014-07-02 15:58:34 +0200

    Port working module

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,47 @@
-describe('indicator', function(){});
+describe('indicator', function(){
+	var MockService = {
+		get: function () {
+			return {
+				measurements: [1, 2],
+				goals: [2, 4]
+			};
+		}
+	}
+
+
+	beforeEach(module('indicator'));
+
+	var el,
+			scope,
+			httpBackend;
+
+	beforeEach(module(function ($provide) {
+		$provide.value('indicatorService', MockService);
+	}))
+
+	beforeEach(inject(function ($rootScope, $compile, $httpBackend){
+		scope = $rootScope.$new();
+		httpBackend = $httpBackend;
+	}))
+
+
+	function compileDirective(tpl){
+		if (!tpl) tpl = '<div indicator-module></div>';
+
+		inject(function ($compile) {
+			el = $compile(tpl)(scope)[0];
+		})
+		scope.$digest();
+	}
+
+
+	describe('initialisation', function () {
+		beforeEach(function () {
+			compileDirective();
+		});
+
+		it('child should have class indicator-module', function () {
+			expect(el.firstElementChild.classList.contains('indicator-module')).toBe(true);
+		});
+	});
+});
